d7a5d5a4834b5fe8a86047139010c48f5f4cbc57
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-20 18:35:09 -0400

    remove unused util methods

--- packages/ws-server/src/litefs/util.ts
+++ packages/ws-server/src/litefs/util.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import os from "os";
 import fs from "fs";
-import { Config } from "../config";
 
 const needsTouchHack = os.platform() === "darwin" || os.platform() === "win32";
 const ex = {
@@ -19,32 +18,11 @@ const ex = {
 
   touchFile(dbpath: string): Promise<void> {
     if (!needsTouchHack) {
-      throw new Error("Touch hack is only required for darwin");
+      throw new Error("Touch hack is only required for darwin and windows");
     }
     return fs.promises.open(ex.getTouchFilename(dbpath), "w").then((fd) => {
       return fd.close();
     });
-  },
-
-  uuidToBytes(uuid: string) {
-    const hex = uuid.replaceAll("-", "");
-    const ret = new Uint8Array(hex.length / 2);
-    for (let c = 0; c < hex.length; c += 2) {
-      ret[c / 2] = parseInt(hex.substring(c, c + 2), 16);
-    }
-    return ret;
-  },
-
-  dbidsAreEqual(a: Uint8Array, b: Uint8Array) {
-    if (a.length !== b.length) {
-      return false;
-    }
-    for (let c = 0; c < a.length; c++) {
-      if (a[c] !== b[c]) {
-        return false;
-      }
-    }
-    return true;
   },
 };
 
