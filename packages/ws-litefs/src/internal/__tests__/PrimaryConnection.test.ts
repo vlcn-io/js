f408aa067b1ab574f1403a7eb4be9a0d61e864d6
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-29 10:17:55 -0400

    test connections are stood up when the priamry file changes

--- packages/ws-litefs/src/internal/__tests__/PrimaryConnection.test.ts
+++ packages/ws-litefs/src/internal/__tests__/PrimaryConnection.test.ts
@@ -2,8 +2,29 @@ import "../../__tests__/testLiteFSConfig.js";
 import { test, expect, afterAll } from "vitest";
 import { createPrimaryConnection } from "../PrimaryConnection";
 import fs from "fs";
+import net from "net";
 
-test("Returns that it is the primary if the primary file is missing", async () => {
+test("Returns that it is the primary if the primary file is missing on construction", async () => {
+  try {
+    fs.rmSync("./test_fs/.primary");
+  } catch (e) {}
+
+  const c = await createPrimaryConnection();
+  expect(c.isPrimary()).toBe(true);
+  c.close();
+});
+
+test("Upgrades self to primary if the primary file exists at construction and is later removed", async () => {
+  fs.writeFileSync("./test_fs/.primary", "test");
+  const c = await createPrimaryConnection();
+  expect(c.isPrimary()).toBe(false);
+  fs.rmSync("./test_fs/.primary");
+  await new Promise((resolve) => setTimeout(resolve, 300));
+  expect(c.isPrimary()).toBe(true);
+  c.close();
+});
+
+test("Downgrades self to follower if the primary file is removed post-construction", async () => {
   try {
     fs.rmSync("./test_fs/.primary");
   } catch (e) {}
@@ -12,20 +33,32 @@ test("Returns that it is the primary if the primary file is missing", async () =
   expect(c.isPrimary()).toBe(true);
 
   fs.writeFileSync("./test_fs/.primary", "test");
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  await new Promise((resolve) => setTimeout(resolve, 300));
   expect(c.isPrimary()).toBe(false);
   fs.rmSync("./test_fs/.primary");
+  c.close();
 });
 
-test("Upgrades self to primary if the primary file exists at construction and is later removed", async () => {
-  fs.writeFileSync("./test_fs/.primary", "test");
+test("Swaps connection when primary changes", async () => {
+  const server = net.createServer();
+  let receivedConn = 0;
+  let receivedClose = 0;
+  server.on("connection", (socket) => {
+    receivedConn += 1;
+  });
+  server.on("closed", () => {
+    receivedClose += 1;
+  });
+  server.listen(9000, "localhost");
+
+  fs.writeFileSync("./test_fs/.primary", "localhost");
   const c = await createPrimaryConnection();
-  expect(c.isPrimary()).toBe(false);
-  fs.rmSync("./test_fs/.primary");
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  expect(receivedConn).toBe(1);
+  expect(receivedClose).toBe(0);
+  fs.writeFileSync("./test_fs/.primary", "127.0.0.1");
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  expect(receivedConn).toBe(2);
+  expect(receivedClose).toBe(1);
+  c.close();
 });
-
-test("Upgrades self to primary if the primary file is created post-construction", () => {});
-
-test("Downgrades self to follower if the primary file is removed post-construction", () => {});
-
-test("Swaps connection when primary changes", () => {});
