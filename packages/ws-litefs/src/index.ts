dd3d718a4f0c8bfa8d7e85666d5dee5704068377
Matt <1009003+tantaman@users.noreply.github.com>
2023-09-06 09:29:10 -0400

    custom polling for litefs

--- packages/ws-litefs/src/index.ts
+++ packages/ws-litefs/src/index.ts
@@ -1,2 +1,3 @@
 export { createLiteFSWriteService } from "./LiteFSWriteService.js";
 export { createLiteFSDBFactory } from "./LiteFSDBFactory.js";
+export { default as FSNotify } from "./FSNotify.js";
