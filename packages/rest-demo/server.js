f7580d8bd94e4e619c8c633d08097e773c549a8e
Matt <1009003+tantaman@users.noreply.github.com>
2023-09-08 11:46:02 -0400

    create db, add dbs folder

--- packages/rest-demo/server.js
+++ packages/rest-demo/server.js
@@ -9,6 +9,11 @@ const PORT = parseInt(process.env.PORT || "8080");
 const app = express();
 const server = http.createServer(app);
 
+app.post("/initialize/:room", () => {
+  const room = req.params.room;
+  const db = createDb(room);
+});
+
 app.get("/changes/:room", (req, res) => {
   const room = req.params.room;
   const sinceVersion = req.query.sinceVersion;
@@ -28,6 +33,37 @@ server.listen(PORT, () =>
 
 ViteExpress.bind(app, server);
 
-class SyncedDB {
-  constructor() {}
+// In an ideal world, you should cache the DB instance so you do not need to pay
+// the cost of re-constructing it (initializing SQLite, loading the cr-sqlite extension) every request.
+// You could also prepare the statements once and cache them in that world.
+// That is how the websocket and direct-connect servers work.
+function createDb(room) {
+  const dbpath = getDbPath(room);
+  const db = new Database(dbpath);
+  db.pragma("journal_mode = WAL");
+  db.pragma("synchronous = NORMAL");
+  db.loadExtension(extensionPath);
+  return db;
+}
+
+const dbFolder = "./dbs";
+const schemaFolder = "./src/schemas";
+function getDbPath(dbName) {
+  if (hasPathParts(dbName)) {
+    throw new Error(`${dbName} must not include '..', '/', or '\\'`);
+  }
+
+  return path.join(config.dbFolder, dbName);
+}
+
+function getSchemaPath(schemaName) {
+  if (hasPathParts(schemaName)) {
+    throw new Error(`${schemaName} must not include '..', '/', or '\\'`);
+  }
+
+  return path.join(config.schemaFolder, schemaName);
+}
+
+function hasPathParts(s) {
+  return s.includes("..") || s.includes("/") || s.includes("\\");
 }
